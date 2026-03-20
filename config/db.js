4c1fba77a6c3062a191a6fda50cd9d4deec815bc
Snehasis Bar <barsnehasis7@gmail.com>
2026-03-21 04:54:46 +0530

    fix: supabase prepare false for serverless

--- config/db.js
+++ config/db.js
@@ -2,13 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-const connectionString = process.env.DATABASE_URL
-
-const client = postgres(connectionString, {
-  ssl: 'require',
-  max: 1,
-  idle_timeout: 20,
-  connect_timeout: 10,
+const client = postgres(process.env.DATABASE_URL, {
+  prepare: false,
+  ssl: { rejectUnauthorized: false },
 })
 
 export const db = drizzle(client, { schema })
